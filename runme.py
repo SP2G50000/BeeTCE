90bbf2ea0e990ca653671e5a945e60eaea6ca6f3
SP2G50000 <superweebs5000@gmail.com>
2021-02-21 21:32:51 -0600

    Added configparser
    The tool now remembers your last applied settings.

--- runme.py
+++ runme.py
@@ -1,11 +1,32 @@
-import sys, string, os, subprocess, shutil
-from PySide6 import QtCore, QtGui
+import sys, os, subprocess
+from PySide6 import QtCore
 from PySide6 import QtWidgets
-from PySide6.QtWidgets import QApplication, QErrorMessage, QFileDialog, QMainWindow
+from PySide6.QtWidgets import QApplication, QFileDialog, QMainWindow
 from PySide6.QtCore import QFile, QIODevice
 from ui_form import Ui_MainWindow
 compile_radial_option = 0
 vrad_option = "-fast"
+#Sets up the configuration file.
+import configparser
+config = configparser.ConfigParser()
+try:
+    config.read('config.ini')
+    p2_config = config["SETTINGS"]["Portal2FilePath"]
+    p2ce_config = config["SETTINGS"]["P2CEFilePath"]
+    vmf_config = config["SETTINGS"]["VMFFilePath"]
+    print("Config file detected. Settings Applied.")
+except:
+    if not config.has_section("SETTINGS"):
+        config.add_section("SETTINGS")
+        config.set("SETTINGS", "Portal2FilePath", "")
+        config.set("SETTINGS", "P2CEFilePath", "")
+        config.set("SETTINGS", "VMFFilePath", "")
+        with open("config.ini", 'w') as configfile:
+            config.write(configfile)
+        print("Created new config file.")
+        p2_config = config["SETTINGS"]["Portal2FilePath"]
+        p2ce_config = config["SETTINGS"]["P2CEFilePath"]
+        vmf_config = config["SETTINGS"]["VMFFilePath"]
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -28,15 +49,25 @@ class MainWindow(QMainWindow):
         self.p2ce_picked = 0
         self.vmf_picked = 0
         self.radial_button_picked = 0
+        global p2_config
+        global p2ce_config
+        global vmf_config
+        self.ui.p2_lineEdit.setText(p2_config)
+        self.ui.p2ce_lineEdit.setText(p2ce_config)
+        self.ui.vmf_lineEdit.setText(vmf_config)
     
     @QtCore.Slot() #This is where most of the UI functions go.
-    def p2_browse_function(self):
+    def p2_browse_function(self): #This function is currently unused.
         dialog = QFileDialog(self)
         dialog.setFileMode(QFileDialog.Directory)
         dialog.setViewMode(QFileDialog.Detail)
         if dialog.exec_():
             p2_fileNames = dialog.selectedFiles()
             p2_fileNames = "".join(str(x) for x in p2_fileNames)
+            global config
+            config.set("SETTINGS", "Portal2FilePath", p2_fileNames)
+            with open("config.ini", 'w') as configfile:
+                config.write(configfile)
             print("You picked",p2_fileNames)
             self.ui.p2_lineEdit.setText(p2_fileNames)
         self.p2_picked = 1
@@ -48,6 +79,10 @@ class MainWindow(QMainWindow):
         if dialog.exec_():
             self.p2ce_fileNames = dialog.selectedFiles()
             self.p2ce_fileNames = "".join(str(x) for x in self.p2ce_fileNames)
+            global config
+            config.set("SETTINGS", "P2CEFilePath", self.p2ce_fileNames)
+            with open("config.ini", 'w') as configfile:
+                config.write(configfile)
             print("You picked",self.p2ce_fileNames)
             self.ui.p2ce_lineEdit.setText(self.p2ce_fileNames)
             self.p2ce_picked = 1
@@ -60,6 +95,10 @@ class MainWindow(QMainWindow):
         if dialog.exec_():
             self.vmf_fileNames = dialog.selectedFiles()
             self.vmf_fileNames = "".join(str(x) for x in self.vmf_fileNames)
+            global config
+            config.set("SETTINGS", "VMFFilePath", self.vmf_fileNames)
+            with open("config.ini", 'w') as configfile:
+                config.write(configfile)
             print("You picked",self.vmf_fileNames)
             self.ui.vmf_lineEdit.setText(self.vmf_fileNames)
             self.vmf_picked = 1
